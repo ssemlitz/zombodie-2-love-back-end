618e53951e47f7574a6665637ff9968a3d4bb220
JakeD <jakediefenderfer@gmail.com>
2022-07-15 13:59:58 -0400

    conversations route

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import formData from 'express-form-data'
 
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as conversationsRouter } from './routes/conversations.js'
+
 
 import './config/database.js'
 
@@ -18,6 +20,7 @@ app.use(formData.parse())
 
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/conversations', conversationsRouter)
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: 'Not found' })
