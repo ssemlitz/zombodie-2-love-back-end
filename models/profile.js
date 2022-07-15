1da0ea45aec7fc8e028d668829e0d9650e9e1150
JakeD <jakediefenderfer@gmail.com>
2022-07-15 11:21:24 -0400

    add properties to profile model

--- models/profile.js
+++ models/profile.js
@@ -3,7 +3,14 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
-  photo: { type: String }
+  photo: { type: String },
+  species: { type: String, enum: ['human', 'zombie', 'halfbies']},
+  brains: Boolean,
+  age: Number,
+  height: Number,
+  bio: String,
+  swiped: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  liked: {type: Schema.Types.ObjectId, ref: 'Profile'},
 },{
   timestamps: true,
 })
