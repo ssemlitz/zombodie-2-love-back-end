2cb14f3f586c4e3fb04b892feab9c84db584e83a
ssemlitz <sdsemlitz@yahoo.com>
2022-07-16 18:49:16 -0400

    refactor conversation model

--- models/conversation.js
+++ models/conversation.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 
 const messageSchema = new mongoose.Schema({
-  text: String,
-  author: ObjectId
+  conversationId: {type: String},
+  sender: {type: String},
+  text: {type: String}
 })
 
 const conversationSchema = new mongoose.Schema({
-  participants: [{type: Schema.types.ObjectId, ref: 'Profile'}],
+  participants: {type: Array},
   messages: [messageSchema],
 }, {
   timestamps: true
